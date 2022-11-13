9fabe7325eda6a789cf2f1d27032580e960cb62b
shazaAly <shaza.aly@gmail.com>
2022-11-13 17:32:44 +0200

    remove from cart

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,18 +1,57 @@
+import { useSelector } from "react-redux";
+
 const { createSlice } = require("@reduxjs/toolkit");
 
-const initialState ={
+const initialState = {
+    items: [],
+    totalQuantity: 0,
 
 }
 
 const cartSlice = createSlice({
-    name : 'cart',
+    name: 'cart',
     initialState,
-    reducers:{
-        addToCart(){},
-        removeItemFromCart(){},
-        RemoveAll(){},
-        getItemsPrice(){},
-        getTotal(){},
+    reducers: {
+        addItemToCart(state, action) {
+            const newItem = action.payload
+            // check if item previously added so increase its count//
+            const existingItem = state.items.find(item => item.id === newItem.id)
+            if (!existingItem) {
+                state.items.push({
+                    itemId: newItem.id,
+                    name: newItem.title,
+                    quantity: newItem.quantity,
+                    price: newItem.price,
+                    totalPrice: newItem.price
+                })
+
+            } else {
+                existingItem.quantity++
+                existingItem.totalPrice = existingItem.totalPrice + existingItem.price
+
+            }
+
+        },
+        removeItemFromCart(state, action) {
+            const newItem = action.payload
+            const existingItem = state.items.find(item => item.id === newItem.id)
+            if (existingItem) {
+                //if quant = 1 remove item from items[], if more decrease it by one //
+                
+                existingItem.item === 1 ? 
+                state.items.filter(item => item.id != existingItem.id) : existingItem.quantity --
+                
+
+            } else {
+                return Error('Item is not in Cart')
+            }
+
+
+
+        },
+        RemoveAll() { },
+        getItemsPrice() { },
+        getTotal() { },
     }
 
 })
